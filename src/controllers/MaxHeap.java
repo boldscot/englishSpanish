6a3ac3759784bf262e50d81a274836019a4fc181
boldscot <>
2016-04-06 22:05:52 +0100

    Final version.

--- src/controllers/MaxHeap.java
+++ src/controllers/MaxHeap.java
@@ -1,3 +1,8 @@
+/**
+ * @author      Stephen Collins 20061696
+ * @Date		06/04/2016
+ */
+
 package controllers;
 
 import java.util.ArrayList;
@@ -6,7 +11,6 @@ import java.util.Stack;
 
 import utils.Serializer;
 import models.Data;
-
 
 public class MaxHeap implements MaxHeapInterface {
 
@@ -30,6 +34,13 @@ public class MaxHeap implements MaxHeapInterface {
 		serializer.write(); 
 	}
 
+	/*
+	 * Stack implementation of post order traversal, the first node in the array is pushed to the stack
+	 * then while the satck is not empty the node being checked is popped from the stack. The node is checked 
+	 * to see if it contains the word being searched, if it is print to screen. If the node has a right or left
+	 * child they are pushed to the stack, the right node is pushed first because the left nodes must be visited
+	 * first in postorder traversal.
+	 */
 	public void findSpanishWord(String englishWord) {
 		int root = 0, traversals = 0;
 		Data currentNode = null;
@@ -56,33 +67,56 @@ public class MaxHeap implements MaxHeapInterface {
 		}
 	} 
 
+	/*
+	 * This method checks if a node has a left child.
+	 */
 	private boolean hasLeftChild(int node) {
 		return (2 * node + 1) < wordData.size();
 	}
 
+	/*
+	 * This method checks if a node has a right child.
+	 */
 	private boolean hasRightChild(int node) {
 		return( 2 * node + 2) < wordData.size();
 	}
 
+	/*
+	 * This method checks if a nodes data contains a word being searched.
+	 */
 	private boolean isWord (Data node, String englishWord) {
 		return node.getEnglish().contains(englishWord);
 	}
 
+	/*
+	 * This method creates a new entry, paases it to the add method and then the siftUp method 
+	 * moves it up the tree to it's correct position.
+	 */
 	public void createDataEntry(String spanish, String english) {
 		int weight = spanish.length() + english .length();
 		add(new Data(spanish, english, weight) );
 		siftUp();
 	}
 
+	/*
+	 * This method returns the contents of the wordData array.
+	 */
 	public List<Data> getDataEntries() {
 		return wordData;
 	}
 
+	/*
+	 * This method adds a new entry to the wordData arrau.
+	 */
 	@Override
 	public void add(Data newEntry) {
 		wordData.add(newEntry);
 	}
-
+	
+	/*
+	 * This method removes the item at the to of the tree (index 0), then the lowest most right node is 
+	 * put in it's position. The sift down method moves the node back ldown the tree to it's correct position.
+	 */
 	@Override
 	public Data removeMax() {
 		if (wordData.size() == 0) {
@@ -97,6 +131,11 @@ public class MaxHeap implements MaxHeapInterface {
 		return maxNode;
 	}
 
+	/*
+	 * This method moves a node up the tree starting at the bottom, the lowest node is compared with it's
+	 * parent and if its weight is greater they're swapped. The loop continues until the data to sift 
+	 * is the root node.
+	 */
 	public void siftUp() {
 		int dataToSift = wordData.size() - 1;
 		while (dataToSift > 0) {
@@ -141,25 +180,35 @@ public class MaxHeap implements MaxHeapInterface {
 		}
 	}
 
+	/*
+	 * This method returns the node at index 0 (root node).
+	 */
 	@Override
 	public Data getMax() {
 		return wordData.get(0);
 	}
 
+	/*
+	 * This method checks if the array is empty or not.
+	 */
 	@Override
 	public boolean isEmpty() {
 		return wordData.isEmpty();
 	}
 
+	/*
+	 * This method retunrs the size of the array.
+	 */
 	@Override
 	public int getSize() {
 		return wordData.size();
 	}
 
+	/*
+	 * This method clears the array.
+	 */
 	@Override
 	public void clear() {
 		wordData.clear();
 	}
-
-
 }
--- src/utils/Serializer.java
+++ src/utils/Serializer.java
@@ -1,10 +1,8 @@
 package utils;
 
-public interface Serializer
-{
+public interface Serializer {
 	void push(Object o);
 	Object pop();
 	void write() throws Exception;
 	void read() throws Exception;
-	
 }
